cbe8c6bd2ec64693e70d141bf934aa6a3bfe1835
rakesh1423 <rakeshkumarpatra1423@gmail.com>
2025-05-30 15:50:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+// import About from './pages/About';
+// import EventDetails from './pages/EventDetails';
+// import RSVP from './pages/RSVP';
+// import SalesPage from './pages/SalesPage';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      
+      <main >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} />
+          <Route path="/details" element={<EventDetails />} />
+          <Route path="/rsvp" element={<RSVP />} />
+          <Route path="/sales" element={<SalesPage />} /> */}
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/Logo.png'; // Adjust the path if your Logo.png is elsewhere
+import './Navbar.css'; // Make sure to create this CSS file
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Logo and Name */}
+        <div className="navbar-logo">
+          <img src={logo} alt="Health and Pharma Logo" className="logo-icon" />
+          <span className="logo-name">Health and Pharma</span>
+        </div>
+        {/* Navigation Links */}
+        <div className="navbar-links">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/details" className="nav-link">Event Details</Link>
+          <Link to="/rsvp" className="nav-link">RSVP</Link>
+          <Link to="/pricing" className="nav-link pricing-btn">Pricing</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
